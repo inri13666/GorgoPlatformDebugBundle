7e63a5b836d23314ad7c73cf02354d4d94399c4a
Vladimir Tsykun <tsykun314@gmail.com>
2017-06-27 12:53:42 +0300

    Increase verbosity level for MQ. (#3)
    - added optional debug option for command oro:message-queue:consume to display all logs in console

--- GorgoPlatformDebugBundle.php
+++ GorgoPlatformDebugBundle.php
@@ -2,8 +2,17 @@
 
 namespace Gorgo\Bundle\PlatformDebugBundle;
 
+use Gorgo\Bundle\PlatformDebugBundle\DependencyInjection\CompilerPass\MessageQueueDebugPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class GorgoPlatformDebugBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new MessageQueueDebugPass());
+    }
 }
